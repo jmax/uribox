72a5c0c222231ad0ef207466579ed171433e1997
Juan Maria Martinez Arce <juanmariamartinezarce@Johns-MacBook-Air.local>
2012-08-14 17:10:12 -0300

    [First Commit]
      - Rails Application Bootstrap
        The bootstrap was carried out using AppScrolls gem with the following command:
    
        scrolls new uribox -s jquery sqlite3 passenger twitter_bootstrap rspec capybara

---
+++ Gemfile
@@ -0,0 +1,21 @@
+
+source 'https://rubygems.org'
+
+gem "rails",                    '3.2.8'
+
+group :development, :test do
+  gem "sqlite3"
+  gem "rspec-rails"
+  gem "capybara"
+end
+
+group :assets do
+  gem "sass-rails",             '~> 3.2.3'
+  gem "coffee-rails",           '~> 3.2.1'
+  gem "twitter-bootstrap-rails"
+  gem "therubyracer"
+  gem "uglifier",               '>= 1.0.3'
+end
+
+gem "jquery-rails"
+gem "simple_form"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uribox::Application.config.secret_token = '8c8ffa8c5465d85601d27287a8998be97e9f33c235978db0f4b73319de6740acb7ed4bc7e44f34f6da5785632030ee05f9edb2999cc312556df570066b64c1a4'
